fd344ec6ae05773504f2f2425d44ba80411a5d7c
Omkarr21 <jadhavomkar553@gmail.com>
2024-07-20 14:13:20 +0530

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,8 @@
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int TeamId { get; set; }
+    public Team Team { get; set; }
+    public ICollection<Task> Tasks { get; set; }
+}
